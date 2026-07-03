795ad9751bf55249825fd53ef60f9dc757dc9fe2
Diego Santos <diegosantos.engtads@gmail.com>
2026-07-03 12:14:18 -0300

    feat(page): initial components in pageHome

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import { SpinLoader } from '@/components/layout/SpinLoader'
+import { PostFeature } from '@/components/posts/PostFeature'
+import { PostsList } from '@/components/posts/PostsList'
+import { Suspense } from 'react'
+
 export default function HomePage() {
   return (
-    <div>
-      <h1>contetudo</h1>
-    </div>
-  );
+    <>
+      <Suspense fallback={<SpinLoader />}>
+        <PostFeature />
+      </Suspense>
+
+      <Suspense fallback={<SpinLoader />}>
+        <PostsList />
+      </Suspense>
+    </>
+  )
 }
