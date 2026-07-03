d64d578564033fcb4367088bdc9b943906103f07
Diego Santos <diegosantos.engtads@gmail.com>
2026-07-03 12:06:53 -0300

    feat(layout): updates to initial layout settings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,37 +1,37 @@
-import { Footer } from "@/components/layout/Footer";
+import { Footer } from '@/components/layout/Footer'
 
-import { Header } from "@/components/layout/Header";
-import type { Metadata } from "next";
-import { Roboto } from "next/font/google";
-import "./globals.css";
+import { Header } from '@/components/layout/Header'
+import type { Metadata } from 'next'
+import { Roboto } from 'next/font/google'
+import './globals.css'
 
 const roboto = Roboto({
-  variable: "--font-roboto",
-  subsets: ["latin"],
-});
+  variable: '--font-roboto',
+  subsets: ['latin'],
+})
 
 export const metadata: Metadata = {
   title: {
-    template: "%s | Nexus",
-    default: "Nexus",
+    template: '%s | Nexus',
+    default: 'Nexus',
   },
-  description: "Nexus",
-};
+  description: 'Nexus',
+}
 
 type RootLayoutProps = {
-  children: React.ReactNode;
-};
+  children: React.ReactNode
+}
 
 const RootLayout = ({ children }: Readonly<RootLayoutProps>) => {
   return (
-    <html lang="pt-BR" className={roboto.variable}>
-      <body className="grid min-h-screen w-full grid-rows-[auto_1fr_auto]">
+    <html lang='pt-BR' className={roboto.variable}>
+      <body className='grid grid-rows-[auto_1fr_auto] min-h-screen w-full'>
         <Header />
-        <main>{children}</main>
+        <main className='bg-slate-100'>{children}</main>
         <Footer />
       </body>
     </html>
-  );
-};
+  )
+}
 
-export default RootLayout;
+export default RootLayout
