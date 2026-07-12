c2a2e6059bff71eefe8a0c379e795f2051eeb835
Diego Santos <diegosantos.engtads@gmail.com>
2026-07-12 11:42:07 -0300

    fix(layout): adjust container width and image sizing

--- src/components/layout/Footer/index.tsx
+++ src/components/layout/Footer/index.tsx
@@ -4,14 +4,13 @@ import styles from './styles.module.css'
 export const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <div className={`${styles.footerInner} container`}>
+      <div className={`${styles.footerInner} container `}>
         <Image
           src='/images/logo.png'
           alt='Logo Nexus'
           width={120}
           height={120}
           loading='eager'
-          className='m-4'
         />
       </div>
     </footer>
